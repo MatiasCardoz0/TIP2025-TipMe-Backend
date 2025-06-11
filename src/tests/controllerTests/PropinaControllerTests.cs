08415d390e1a0dbdf06ae4c48dafb33b44d8213b
camila-am <marchinicamila94@gmail.com>
2025-06-11 00:03:29 -0300

    Arreglo imports para que corran los test

--- src/tests/controllerTests/PropinaControllerTests.cs
+++ src/tests/controllerTests/PropinaControllerTests.cs
@@ -8,32 +8,35 @@ using TipMeBackend.Models;
 using TipMeBackend.Controllers.DTOs;
 using TipMeBackend.Data.PropinaRepository;
 
-[TestFixture]
-public class PropinaControllerTests
+namespace TipMeBackend.Tests.ControllerTests
 {
-    [Test]
-    public async Task Grabar_propina_devuelve_Ok_si_es_llamado_exitoso()
+    [TestFixture]
+    public class PropinaControllerTests
     {
-        var propinaService = new Mock<IPropinaService>();
-        propinaService.Setup(s => s.GrabarPropina(It.IsAny<PropinaDTO>()))
-            .ReturnsAsync(new Response<string>("ok", 200));
-        var controller = new PropinaController(propinaService.Object);
+        [Test]
+        public async Task Grabar_propina_devuelve_Ok_si_es_llamado_exitoso()
+        {
+            var propinaService = new Mock<IPropinaService>();
+            propinaService.Setup(s => s.GrabarPropina(It.IsAny<PropinaDTO>()))
+                .ReturnsAsync(new Response<string>("ok", 200));
+            var controller = new PropinaController(propinaService.Object);
 
-        var result = await controller.grabarPropina(new PropinaDTO());
+            var result = await controller.grabarPropina(new PropinaDTO());
 
-        ClassicAssert.IsInstanceOf<OkObjectResult>(result);
-    }
+            ClassicAssert.IsInstanceOf<OkObjectResult>(result);
+        }
 
-    [Test]
-    public async Task Obtener_propinas_devuelve_Ok_si_es_llamado_exitoso()
-    {
-        var propinaService = new Mock<IPropinaService>();
-        propinaService.Setup(s => s.ObtenerPropinas(1))
-            .ReturnsAsync(new Response<System.Collections.Generic.List<PropinaDTOGet>>(new System.Collections.Generic.List<PropinaDTOGet>(), 200));
-        var controller = new PropinaController(propinaService.Object);
+        [Test]
+        public async Task Obtener_propinas_devuelve_Ok_si_es_llamado_exitoso()
+        {
+            var propinaService = new Mock<IPropinaService>();
+            propinaService.Setup(s => s.ObtenerPropinas(1))
+                .ReturnsAsync(new Response<System.Collections.Generic.List<PropinaDTOGet>>(new System.Collections.Generic.List<PropinaDTOGet>(), 200));
+            var controller = new PropinaController(propinaService.Object);
 
-        var result = await controller.getPropinas(1);
+            var result = await controller.getPropinas(1);
 
-        ClassicAssert.IsInstanceOf<OkObjectResult>(result);
+            ClassicAssert.IsInstanceOf<OkObjectResult>(result);
+        }
     }
 }
